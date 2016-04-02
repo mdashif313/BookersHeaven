6951ac15a2dd9de2886503b29642d6b1afc2c134
Md.Ashif Al Nowajesh <mdashifalnowajesh@gmail.com>
2016-04-02 07:38:10 -0600

    Update DBHelper.php

--- DBHelper.php
+++ DBHelper.php
@@ -26,6 +26,50 @@ class DBHelper {
     }
     
     
+    /*
+     * this function generate random
+     * key to secure user session
+     */
+    function DbHash($password){
+        return rtrim(base64_encode(hash('gost-crypto',microtime().rand().$password)
+                .md5(microtime()+rand().'fgwguwieh0589h573hc29rt3htg')
+                .hash('ripemd160', microtime().'34b5ljhbfb472Dy93HcDQWE NFfeuyhfeBcd'.microtime())),"=");
+    }
+    
+    
+    /*
+     * this function query & return 
+     * the hash string for spacific 
+     * user email from database
+     */
+    function DbGetHash($xml){
+        $user_email = $xml->user_email;
+        
+        $sql = "SELECT * FROM user WHERE user_email = '$user_email'";
+        $query = $this->conn->query($sql);
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        
+        $hash = '<hash>';
+        $hash = $hash.'<key>'.$row['hash'].'</key>';
+        $hash = $hash.'</hash>';      
+        
+        $simplexml = simplexml_load_string($hash);
+        return $simplexml;
+    }
+
+    
+    function DbSignUp(){
+        $hash = $this->DbHash($password);
+        $sqlHash = "UPDATE user SET hash='$hash' WHERE user_email = '$user_email'";
+        $this->conn->exec($sqlHash);
+    }
+
+
+
+    /*
+     * this is dataaccess layer function
+     * to check log in email & password
+     */    
     function DbLogIn($xml){
         $user_email = $xml->user_email;
         $password = $xml->password;
@@ -35,9 +79,7 @@ class DBHelper {
         
         if($query->rowCount()==0){
             $result = '<status>
-                        <password>
-                        Invalid
-                        </password>
+                        <password>Invalid</password>
                       </status>'; 
             echo 'Fail'.$query->rowCount();
             $simplexml = simplexml_load_string($result);
@@ -47,9 +89,7 @@ class DBHelper {
         
         if($row['user_status']==0){
             $result = '<status>
-                        <password>
-                        Inactive
-                        </password>
+                        <password>Inactive</password>
                       </status>'; 
             echo 'Inactive'.$query->rowCount();
             $simplexml = simplexml_load_string($result);
@@ -58,22 +98,17 @@ class DBHelper {
         
         if($row['password']==$password){
             $result = '<status>
-                        <password>
-                        Correct
-                        </password>
+                        <password>Valid</password>
                       </status>'; 
-            echo 'Correct'.$query->rowCount();
+            echo 'Correct'.$query->rowCount();            
             $simplexml = simplexml_load_string($result);
             return $simplexml;
         }
         $result = '<status>
-                <password>
-                true
-                </password>
+                <password>Invalid</password>
               </status>'; 
         echo 'Incorrect'.$query->rowCount();
         $simplexml = simplexml_load_string($result);
-        return $simplexml;
-                        
+        return $simplexml;                        
     }
 }
