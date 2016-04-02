d05c87dd8c8d41836fc340804b7687810c681903
Md.Ashif Al Nowajesh <mdashifalnowajesh@gmail.com>
2016-04-02 07:39:24 -0600

    Create test.php

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//require_once '../DataAccess/DBHelper.php';
+  
+    /*require_once 'User.php';
+    $user = new User();
+
+    $user_email = 'abul@babul.com';
+    $password = '12345';
+    $xml = '<login>';
+    $xml = $xml.'<user_email>'.$user_email.'</user_email>';
+    $xml = $xml.'<password>'.$password.'</password>';
+    $xml = $xml.'</login>';
+
+    $simplexml = simplexml_load_string($xml) or die("Error: Cannot create object");
+    $user->IsLoggedIn($simplexml);
+*/
+
+    $str = ""+microtime();
+    echo $str;
+     
+        
